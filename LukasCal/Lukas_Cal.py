462b302dec9f437b2c516fce742d0c6a598c5614
rashdotinc <42000824+robertoash@users.noreply.github.com>
2020-06-24 13:03:58 +0200

    Lukas_Cal updated and added to piAppSwitcher

--- LukasCal/Lukas_Cal.py
+++ LukasCal/Lukas_Cal.py
@@ -1,5 +1,53 @@
 # -*- coding: utf-8 -*-
 
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import datetime
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SERVICE_ACCOUNT_FILE = '/var/www/piAppSwitcher/LukasCal/service.json'
+
+credentials = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+
+    service = build('calendar', 'v3', credentials=credentials)
+
+    # Call the Calendar API
+    nowminus24 = (datetime.datetime.utcnow() - datetime.timedelta(hours=24)).isoformat() + 'Z'  # 'Z' indicates UTC time
+    # print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId='im87vdp823ct581abv0dce9s54@group.calendar.google.com',
+                                          timeMin=nowminus24,
+                                          maxResults=1, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    next_events = []
+
+    if not events:
+        return 'No upcoming events found.'
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        # Include only all-day events
+        if len(start) <= 10:
+            next_events.append([start, event['summary']])
+
+    if str(next_events[0][0]).replace("-", "") == datetime.datetime.utcnow().strftime("%Y%m%d"):
+        return 'Vacation'
+    else:
+        return 'Not'
+
+
+# if __name__ == '__main__':
+#     main()
+
+
 def L_Cal():
-    test = "This works"
-    return test
+    return main()
